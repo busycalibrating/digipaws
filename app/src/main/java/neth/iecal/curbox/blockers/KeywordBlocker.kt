5951d4f5bd21b66ec25a60ded5b067b5fbb25348
Francisco Herrera <ppaanncchhoo507@gmail.com>
2026-07-07 00:48:14 -0500

    keyword blocker: double back-press with 250ms delays to prevent browser lockout (#326)
    * keyword blocker: double back-press with 250ms delays to prevent browser lockout
    
    * Clarify comment reflecting pr

--- app/src/main/java/neth/iecal/curbox/blockers/KeywordBlocker.kt
+++ app/src/main/java/neth/iecal/curbox/blockers/KeywordBlocker.kt
@@ -185,8 +185,15 @@ class KeywordBlocker : BaseBlocker() {
     }
 
     private fun handleBlocking(group: KeywordGroup) {
-        service.pressBack()
-        Thread.sleep(1000)
+        //we need three waits for the Chrome ui to update the blocked word in the address bar
+        // so that the user is not locked out of the browser
+        Thread.sleep(250)//if the user touches a home screen shortcut it allows time for the browser UI to update
+        service.pressBack()//if the user presses a Chrome home screen shortcut, this will cause Chrome to exit
+        //to the home screen, if the user is typing a blocked word it will close the keyboard
+        Thread.sleep(250)
+        service.pressBack()//we need a second back press on chrome so that the user is able to type
+        //another URL
+        Thread.sleep(250)
         service.pressHome()
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(service, WarningActivity::class.java).apply {
